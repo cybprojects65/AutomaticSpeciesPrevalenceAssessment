76517f39bbf80fff1f249d1089bcc81de21a26c3
Pasquale Bove <31758271+bovepas@users.noreply.github.com>
2025-02-28 14:36:19 +0100

    Update 1 GBIF request occurrence in large bounding box.R

--- 1 GBIF request occurrence in large bounding box.R
+++ 1 GBIF request occurrence in large bounding box.R
@@ -17,7 +17,7 @@ poligon <- "MULTIPOLYGON (((19.51 47.46, 6.41 47.46, 6.41 36.21, 19.51 36.21, 19
 for(j in  1:nrow(species)){
   cat(paste0("I retrieve data of species ",species$species[j]," with code ", species$code[j], "\n"))
 
-  gbif_download <- occ_download(pred("taxonKey", species$code[j]),format = "SIMPLE_CSV",user="pasquale.bove",pwd="F5RynWe.a$vk*2S",email="pasquale.bove@cnr.it",pred_gt("year", year),pred_within(poligon))
+  gbif_download <- occ_download(pred("taxonKey", species$code[j]),format = "SIMPLE_CSV",user="",pwd="",email="pasquale.bove@cnr.it",pred_gt("year", year),pred_within(poligon))
   occ_download_wait(gbif_download)
   d <- occ_download_get(gbif_download,overwrite = TRUE) %>% occ_download_import()
   write.csv(d,paste0(outputfolder,species$species[j],"_in_BB.csv"),row.names=FALSE)
